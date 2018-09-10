78a12b7b96ef438a0610d926895ef9d497e5e06a
Caleb Sharp <epicsharp11@gmail.com>
2018-09-10 16:15:31 -0700

    Add EditUserDialog story

---
+++ frontend/src/stories/EditUserDialog.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import EditUserDialog from '../components/EditUserDialog'
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+import { action } from '@storybook/addon-actions'
+
+storiesOf('Edit User Dialog', module)
+  .addDecorator(withKnobs)
+  .add('Default', () => <EditUserDialog open={true} />)
+  .add('With knobs', () => (
+    <EditUserDialog
+      open={boolean('Open', true)}
+      onSave={action('save')}
+      onClose={action('close')}
+    />
+  ))
+  .add('With default values', () => (
+    <EditUserDialog
+      firstName="Caleb"
+      lastName="Sharp"
+      isAdmin={false}
+      avatarUrl="https://api.adorable.io/avatars/285/abott@adorable.png"
+      open={true}
+    />
+  ))
