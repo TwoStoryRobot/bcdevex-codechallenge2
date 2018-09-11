eaba9931e28d93e87f8047f52f136196b64d7ed9
Jonathan Bowers <jonotron@gmail.com>
2018-09-11 10:33:02 -0700

    restructure tests with describe and it for consistency

--- frontend/src/AlertDialog.test.js
+++ frontend/src/AlertDialog.test.js
@@ -5,46 +5,48 @@ import AlertDialog from './AlertDialog'
 
 afterEach(cleanup)
 
-test('should render with required props', async () => {
-  const { getByTestId } = render(
-    <AlertDialog open={true} onCancel={jest.fn()} onConfirm={jest.fn()}>
-      Are you TESTING?
-    </AlertDialog>
-  )
+describe('AlertDialog', () => {
+  it('should render with required props', async () => {
+    const { getByTestId } = render(
+      <AlertDialog open={true} onCancel={jest.fn()} onConfirm={jest.fn()}>
+        Are you TESTING?
+      </AlertDialog>
+    )
 
-  // AlertDialog wraps Dialog which is a modal and renders using a portal
-  // must get by testid to find the element in the document
-  expect(getByTestId('alert-dialog')).toMatchSnapshot()
-})
+    // AlertDialog wraps Dialog which is a modal and renders using a portal
+    // must get by testid to find the element in the document
+    expect(getByTestId('alert-dialog')).toMatchSnapshot()
+  })
 
-test('should call onCancel when labeled cancel clicked', async () => {
-  const mockCancel = jest.fn()
-  const { getByText } = render(
-    <AlertDialog
-      open={true}
-      cancelLabel="TESTCANCEL"
-      onCancel={mockCancel}
-      onConfirm={jest.fn()}
-    />
-  )
+  it('should call onCancel when labeled cancel clicked', async () => {
+    const mockCancel = jest.fn()
+    const { getByText } = render(
+      <AlertDialog
+        open={true}
+        cancelLabel="TESTCANCEL"
+        onCancel={mockCancel}
+        onConfirm={jest.fn()}
+      />
+    )
 
-  fireEvent.click(getByText('TESTCANCEL'))
+    fireEvent.click(getByText('TESTCANCEL'))
 
-  expect(mockCancel).toHaveBeenCalled()
-})
+    expect(mockCancel).toHaveBeenCalled()
+  })
 
-test('should call onConfirm when labeled confirm clicked', async () => {
-  const mockConfirm = jest.fn()
-  const { getByText } = render(
-    <AlertDialog
-      open={true}
-      onCancel={jest.fn()}
-      confirmLabel="TESTCONFIRM"
-      onConfirm={mockConfirm}
-    />
-  )
+  it('should call onConfirm when labeled confirm clicked', async () => {
+    const mockConfirm = jest.fn()
+    const { getByText } = render(
+      <AlertDialog
+        open={true}
+        onCancel={jest.fn()}
+        confirmLabel="TESTCONFIRM"
+        onConfirm={mockConfirm}
+      />
+    )
 
-  fireEvent.click(getByText('TESTCONFIRM'))
+    fireEvent.click(getByText('TESTCONFIRM'))
 
-  expect(mockConfirm).toHaveBeenCalled()
+    expect(mockConfirm).toHaveBeenCalled()
+  })
 })
