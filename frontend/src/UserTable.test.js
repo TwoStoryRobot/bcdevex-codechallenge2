48cb1fef8fce73bedf8b3a241c5e10dbf5b8e7a5
Chad Fawcett <me@chadf.ca>
2018-09-11 11:08:20 -0700

    Add UserTable tests

---
+++ frontend/src/UserTable.test.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { render, cleanup } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+import UserTable from './UserTable'
+
+afterEach(cleanup)
+
+describe('UserTable', () => {
+  const defaultUsers = [
+    {
+      userId: 'testUser123',
+      firstName: 'Bob',
+      lastName: 'Smith',
+      emailAddress: 'bob.smith@gmail.com',
+      imageUrl: 'https://d3iw72m71ie81c.cloudfront.net/male-52.jpg'
+    },
+    {
+      userId: 'testUser234',
+      firstName: 'Jane',
+      lastName: 'Doe',
+      emailAddress: 'jane.doe@gmail.com',
+      imageUrl: 'https://randomuser.me/api/portraits/women/21.jpg'
+    },
+
+    {
+      userId: 'testUser345',
+      firstName: 'James',
+      lastName: 'Bond',
+      emailAddress: 'james.bond@gmail.com',
+      imageUrl: 'https://twostoryrobot.com/not-a-valid-url'
+    }
+  ]
+
+  it('should render with required props', async () => {
+    const { container } = render(<UserTable users={defaultUsers} />)
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
