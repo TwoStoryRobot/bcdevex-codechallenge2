9326fb638640e72bb5934f86d83a61c63218dc97
Caleb Sharp <epicsharp11@gmail.com>
2018-09-11 16:42:43 -0700

    Add AppBar story

---
+++ frontend/src/stories/AppBar.story.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { storiesOf } from '@storybook/react'
+import { host } from 'storybook-host'
+import { withKnobs, number, text } from '@storybook/addon-knobs/react'
+import { action } from '@storybook/addon-actions'
+import docgen from '@twostoryrobot/storybook-addon-docgen'
+
+import AppBar from '../components/AppBar'
+
+storiesOf('App Bar', module)
+  .addDecorator(withKnobs)
+  .addDecorator(docgen(AppBar))
+  .add('default', () => 
+    <AppBar
+      title="Code Challenge"
+      avatar="https://api.adorable.io/avatars/285/abott@adorable.png"
+    />
+  )
+  .add('with knobs', () =>
+    <AppBar
+      title={text('Title', 'Title')}
+      avatar={text('Avatar URL', 'ttps://api.adorable.io/avatars/285/abott@adorable.png')}
+      name={text('Name', 'John Doe')}
+      onEdit={action('edit')}
+      onSignOut={action('sign out')}
+    />
+  )
