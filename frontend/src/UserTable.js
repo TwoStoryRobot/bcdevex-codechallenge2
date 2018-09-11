0d9e5e187970875362713868367644c41d17bd58
Chad Fawcett <me@chadf.ca>
2018-09-11 10:26:17 -0700

    UserTable component

---
+++ frontend/src/UserTable.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Avatar from 'react-avatar'
+import { withStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+
+const StyledTableCell = withStyles(() => ({
+  head: {
+    fontSize: '1.25rem',
+    color: '#9A9898',
+    fontWeight: 400
+  },
+  body: {
+    fontSize: '1.125rem',
+    fontWeight: 300
+  }
+}))(TableCell)
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    margintTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 700
+  }
+})
+
+const UserTable = ({ users }) => (
+  <Table>
+    <TableHead>
+      <TableRow>
+        <StyledTableCell />
+        <StyledTableCell>First</StyledTableCell>
+        <StyledTableCell>Last</StyledTableCell>
+        <StyledTableCell>Email</StyledTableCell>
+        <StyledTableCell>Registration</StyledTableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {users.map(user => (
+        <TableRow key={user.userId}>
+          <StyledTableCell component="th" scope="row">
+            <Avatar
+              key={user.userId}
+              src={user.imageUrl}
+              name={`${user.firstName} ${user.lastName}`}
+              size={34}
+              round={true}
+            />
+          </StyledTableCell>
+          <StyledTableCell>{user.firstName}</StyledTableCell>
+          <StyledTableCell>{user.lastName}</StyledTableCell>
+          <StyledTableCell>{user.emailAddress}</StyledTableCell>
+          {/* TODO This needs to come from the user prop */}
+          {/* Note: We are manually overriding this individual column font size */}
+          <StyledTableCell style={{ fontSize: '0.875rem' }}>
+            2018 Sept 04
+          </StyledTableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
+)
+
+UserTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+  users: PropTypes.arrayOf(
+    PropTypes.shape({
+      userId: PropTypes.string.isRequired,
+      firstName: PropTypes.string.isRequired,
+      lastName: PropTypes.string,
+      emailAddress: PropTypes.emailAddress,
+      imageUrl: PropTypes.string
+    })
+  ).isRequired
+}
+
+UserTable.defaultProps = {
+  users: [
+    {
+      userId: 'testUser123',
+      firstName: 'Bob',
+      lastName: 'Smith',
+      emailAddress: 'bob.smith@gmail.com',
+      imageUrl: 'https://d3iw72m71ie81c.cloudfront.net/male-52.jpg'
+    }
+  ]
+}
+
+export default withStyles(styles)(UserTable)
---
+++ frontend/src/stories/UserTable.story.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { host } from 'storybook-host'
+import { withKnobs } from '@storybook/addon-knobs/react'
+import { action } from '@storybook/addon-actions'
+import docgen from '@twostoryrobot/storybook-addon-docgen'
+
+import UserTable from '../UserTable'
+
+const defaultUsers = [
+  {
+    userId: 'testUser123',
+    firstName: 'Bob',
+    lastName: 'Smith',
+    emailAddress: 'bob.smith@gmail.com',
+    imageUrl: 'https://d3iw72m71ie81c.cloudfront.net/male-52.jpg'
+  },
+  {
+    userId: 'testUser234',
+    firstName: 'Jane',
+    lastName: 'Doe',
+    emailAddress: 'jane.doe@gmail.com',
+    imageUrl: 'https://randomuser.me/api/portraits/women/21.jpg'
+  },
+
+  {
+    userId: 'testUser345',
+    firstName: 'James',
+    lastName: 'Bond',
+    emailAddress: 'james.bond@gmail.com',
+    imageUrl: 'https://twostoryrobot.com/not-a-valid-url'
+  }
+]
+
+storiesOf('UserTable', module)
+  .addDecorator(host())
+  .addDecorator(docgen(UserTable))
+  .add('default', () => {
+    return <UserTable users={defaultUsers} />
+  })
