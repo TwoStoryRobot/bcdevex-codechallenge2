0fca09c3758752e39db10a1c6b67a9b5c0c50efe
Chris Foster <chris.james.foster@gmail.com>
2018-09-11 10:31:10 -0700

    Add private route authentication

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,13 +5,14 @@
 
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
 import Private from './Private'
 import Home from './Home'
 
 const App = () => (
   <Switch>
     <Route exact path='/' component={Home} />
-    <Route exact path='/private' component={Private} />
+    <PrivateRoute exact path='/private' component={Private} />
   </Switch>
 )
 
---
+++ frontend/src/PrivateRoute.js
@@ -0,0 +1,31 @@
+
+/* Private Route
+ * Redirects away from this route if the 
+ * user is not logged into the application
+ */
+
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { UserConsumer } from './UserContext'
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <UserConsumer>
+    {({ isLoggedIn }) => (
+      <Route
+        {...rest}
+        render={props =>
+          isLoggedIn ? (
+            <Component {...props} />
+          ) : (
+            <Redirect to={{
+              pathname: "/",
+              state: { from : props.location } 
+            }} />
+          )
+        }
+      />
+    )}
+  </UserConsumer>
+)
+
+export default PrivateRoute
