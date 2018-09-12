5c0b9c40774cb9319a7b8e04b32ca39b62eacdf2
Chris Foster <chris.james.foster@gmail.com>
2018-09-11 22:22:53 -0700

    Add error validation for attempts to create users with mismatched token ID and for existing users

--- backend/src/router/authenticate.js
+++ backend/src/router/authenticate.js
@@ -9,6 +9,11 @@ import { queries } from '../db'
 const authenticate = Router()
 
 async function authenticateUser(ctx) {
+  const stateUserId = ctx.state.user.sub
+  const bodyUserId = ctx.request.body.userId
+  const record = await queries.selectUserById(stateUserId)
+  if (bodyUserId != stateUserId) ctx.throw(400, 'You can only self-register')
+  if (record) ctx.throw(400, 'You are already registered')
   await queries.insertUser(ctx.request.body)
   ctx.body = ctx.request.body
 }
