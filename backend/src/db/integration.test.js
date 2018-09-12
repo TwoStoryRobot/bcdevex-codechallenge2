79ff688c55453245c9aa75a2dea14fa3de471c24
mccullochk <kaileen.mcculloch@gmail.com>
2018-09-11 19:36:38 -0700

    move generateUser to helper function file

--- backend/src/db/integration.test.js
+++ backend/src/db/integration.test.js
@@ -1,15 +1,5 @@
 import { queries, db, pgp } from './index'
-
-function generateUser(obj = {}) {
-  const defaultUser = {
-    userId: '1',
-    firstName: 'Test',
-    imageURL: 'https://previews.123rf.com/images/triken/triken1608/triken160800028/61320729-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg',
-    emailAddress: 'test@user.com',
-    lastName: 'User'
-  }
-  return Object.assign({}, defaultUser, obj)
-}
+import { generateUser } from '../helpers'
 
 beforeEach(async () => {
   // Start test with a clean db state
