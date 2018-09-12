aa2eeb1cfd6bb25b43bea3eb711c144ec081d92e
mccullochk <kaileen.mcculloch@gmail.com>
2018-09-12 16:46:21 -0700

    add registeredAt field to generateUser

--- backend/src/helpers.js
+++ backend/src/helpers.js
@@ -21,7 +21,8 @@ export function generateUser(obj = {}) {
     imageURL: 'https://previews.123rf.com/images/triken/triken1608/triken160800028/61320729-male-avatar-profile-picture-default-user-avatar-guest-avatar-simply-human-head-vector-illustration-i.jpg',
     emailAddress: 'test@user.com',
     lastName: 'User',
-    isAdmin: false
+    isAdmin: false,
+    registeredAt: '2018-09-12T14:35:38-07:00'
   }
   return Object.assign({}, defaultUser, obj)
 }
