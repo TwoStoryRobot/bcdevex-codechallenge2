8a2f58c7305f713cfd7591e626fbd4cd525ec129
Chad Fawcett <me@chadf.ca>
2018-09-11 23:43:03 -0700

    Update sendEmail tests

--- backend/src/router/router.test.js
+++ backend/src/router/router.test.js
@@ -101,7 +101,3 @@ test('/authenticate should authenticate a valid user', async () => {
 test('/authenticate should reject an invalid user', async () => {
   await request.get(`/authenticate`).expect(501)
 })
-
-test('/sendEmail should dispatch email to provided email address', async () => {
-  await request.post(`/sendEmail`, {}).expect(501)
-})
---
+++ backend/src/router/sendEmail.test.js
@@ -0,0 +1,59 @@
+import app from '../app'
+import { queries, db, pgp } from '../db'
+import { generateUser } from '../helpers'
+import supertest from 'supertest'
+import nodemailer from 'nodemailer'
+
+const server = app.listen()
+const postAgent = supertest
+  .agent(server)
+  .post('/sendEmail')
+  .set('Accept', 'application/json')
+
+beforeEach(async () => {
+  //  Restore mocks to clear any calls
+  jest.restoreAllMocks()
+
+  //  Start test with a clean db state
+  await db.none('TRUNCATE public.user')
+
+  await Promise.all([
+    queries.insertUser(generateUser({ userId: '1' })),
+    queries.insertUser(generateUser({ userId: '2' }))
+  ])
+})
+
+afterAll(async () => {
+  //  Close the server connection
+  server.close()
+
+  //  Return db to clean state
+  await db.none('TRUNCATE public.user')
+
+  //  Close the pgp connection
+  await pgp.end()
+})
+
+test.skip('/sendEmail should require admin role', () => {})
+
+test('/sendEmail should 400 if userId is not provided', async () => {
+  await postAgent.send({}).expect(400)
+})
+
+test('/sendEmail should 404 if user does not exist', async () => {
+  await postAgent.send({ userId: 'not-a-user' }).expect(404)
+})
+
+test('/sendEmail should 400 if user does not have an email address', async () => {
+  const userId = 'noemail'
+  await queries.insertUser(generateUser({ userId, emailAddress: '' }))
+
+  await postAgent.send({ userId }).expect(400)
+})
+
+test('/sendEmail should email user', async () => {
+  await postAgent.send({ userId: '1' }).expect(200)
+
+  nodemailer.sendMail()
+  expect(nodemailer.sendMail).toHaveBeenCalled()
+})
