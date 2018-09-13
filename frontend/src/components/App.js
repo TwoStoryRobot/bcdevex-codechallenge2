d35018f06640d01e926dd991adeacf2e2f308fd4
Chad Fawcett <me@chadf.ca>
2018-09-12 19:39:43 -0700

    Logout component and route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,11 +6,13 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import Home from './Home'
+import Logout from './Logout'
 import RegisteredUsers from './RegisteredUsers'
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/logout" component={Logout} />
     <PrivateRoute exact path="/users" component={RegisteredUsers} />
   </Switch>
 )
