d0a5eece65a71a8f10a442d11e42b9717d1f8243
Caleb Sharp <epicsharp11@gmail.com>
2018-09-12 18:19:42 -0700

    Add catch all route

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -7,11 +7,13 @@ import { Route, Switch } from 'react-router-dom'
 import PrivateRoute from './PrivateRoute'
 import Home from './Home'
 import RegisteredUsers from './RegisteredUsers'
+import NotFound from './NotFound'
 
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <PrivateRoute exact path="/users" component={RegisteredUsers} />
+    <Route component={NotFound} />
   </Switch>
 )
 
